64a8954ba9023760b8889423cd8e650532975eb0
Adam Rutherford-Shaw <adamrutherfordshaw@gmail.com>
2023-12-05 12:15:00 +0000

    refactor: move time counter dispatch to component

--- src/components/atoms/counter/counter.tsx
+++ src/components/atoms/counter/counter.tsx
@@ -1,9 +1,16 @@
-import { XmasHackStateContext } from '@/context/context/context';
-import { useContext } from 'react'
+import { incrementTimeDays } from '@/context/actions/example/hello-world';
+import { XmasHackDispatchContext, XmasHackStateContext } from '@/context/context/context';
+import { useContext, useEffect } from 'react'
 
 export const TimeCounter = () => {
-
+    const dispatch = useContext(XmasHackDispatchContext);
     const { timeDays } = useContext(XmasHackStateContext);
+
+    useEffect(() => {
+        const interval = setInterval(() => dispatch(incrementTimeDays()), 1000);
+
+        return () => clearInterval(interval);
+    }, [dispatch]);
 
     return (
         <div>{timeDays} days</div>
