8c0f34c341e0487fd75a936977f09690e8199567
Nick Bansal <nick.bansal@autotrader.co.uk>
2023-12-05 09:58:46 +0000

    started with the basic car card component

---
+++ src/components/molecules/basic-car-card.tsx
@@ -0,0 +1,29 @@
+export const BasicCarCard = () => {
+  //   const [timer, setTimer] = useState(0);
+  //   const timeout = useRef<NodeJS.Timeout | null>(null);
+
+  //   useEffect(() => {
+  //     startTimer();
+  //     return () => {
+  //       clearInterval(timeout.current as NodeJS.Timeout);
+  //     };
+  //   }, []);
+
+  //   const resetTimer = () => {
+  //     clearInterval(timeout.current as NodeJS.Timeout);
+  //   };
+
+  //   const startTimer = () => {
+  //     const maxAmount = TESTIMONIALS.length;
+  //     timeout.current = setInterval(() => {
+  //       setSlide((prevState) => {
+  //         if (prevState === maxAmount - 1) {
+  //           return 0;
+  //         }
+  //         return prevState + 1;
+  //       });
+  //     }, 5000);
+  //   };
+
+  return <div>Hello</div>;
+};
