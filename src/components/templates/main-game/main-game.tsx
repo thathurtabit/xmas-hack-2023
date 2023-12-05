cde6d3a991c6a2800fbc17399f814cbd25c89145
Nick Bansal <nick.bansal@autotrader.co.uk>
2023-12-05 12:04:31 +0000

    added masic cards into main game

--- src/components/templates/main-game/main-game.tsx
+++ src/components/templates/main-game/main-game.tsx
@@ -1,4 +1,5 @@
 import { TimeCounter } from "@/components/atoms/counter/counter";
+import { BasicCarCard } from "@/components/molecules/basic-card-component";
 import { Header } from "@/components/molecules/header/header";
 import { setHelloWorld } from "@/context/actions/example/hello-world";
 import {
@@ -8,20 +9,26 @@ import {
 import { useContext } from "react";
 
 export const MainGame = () => {
-    const dispatch = useContext(XmasHackDispatchContext);
-    const { helloWorld } = useContext(XmasHackStateContext);
-    const handleHelloWorld = () => {
-        dispatch(setHelloWorld(!helloWorld));
-    };
+  const dispatch = useContext(XmasHackDispatchContext);
+  const { helloWorld } = useContext(XmasHackStateContext);
+  const handleHelloWorld = () => {
+    dispatch(setHelloWorld(!helloWorld));
+  };
 
-    return (
-        <>
-            <Header />
-            <TimeCounter />
-            <h1>This is the main game screen</h1>
-            <button className="btn-primary btn" onClick={handleHelloWorld}>
-                Value of Hello World: "{helloWorld.toString()}" - click to change
-            </button>
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <TimeCounter />
+      <h1>This is the main game screen</h1>
+      <button className="btn-primary btn" onClick={handleHelloWorld}>
+        Value of Hello World: "{helloWorld.toString()}" - click to change
+      </button>
+
+      <div className="flex justify-center space-x-4">
+        <BasicCarCard starting={55} max={65} min={42} />
+        <BasicCarCard starting={62} max={69} min={49} />
+        <BasicCarCard starting={23} max={10} min={45} />
+      </div>
+    </>
+  );
+};
