e82ba08e3a87d3ccc5837949fecbd2f63fab8cb5
Natalie Brady <natalie.brady@autotrader.co.uk>
2023-12-05 11:50:58 +0000

    move timercounter into main game template

--- src/components/templates/main-game.tsx
+++ src/components/templates/main-game.tsx
@@ -1,7 +1,25 @@
+import { TimeCounter } from "@/components/atoms/counter/counter";
+import { setHelloWorld } from "@/context/actions/example/hello-world";
+import {
+  XmasHackDispatchContext,
+  XmasHackStateContext,
+} from "@/context/context/context";
+import { useContext } from "react";
+
 export const MainGame = () => {
+    const dispatch = useContext(XmasHackDispatchContext);
+    const { helloWorld } = useContext(XmasHackStateContext);
+    const handleHelloWorld = () => {
+        dispatch(setHelloWorld(!helloWorld));
+    };
+
     return (
-        <div>
+        <>
+            <TimeCounter />
             <h1>This is the main game screen</h1>
-        </div>
+            <button className="btn-primary btn" onClick={handleHelloWorld}>
+                Value of Hello World: "{helloWorld.toString()}" - click to change
+            </button>
+        </>
     )
 }
--- src/pages/Home/ui/Page/Page.tsx
+++ src/pages/Home/ui/Page/Page.tsx
@@ -1,25 +1,11 @@
-import { TimeCounter } from "@/components/atoms/counter/counter";
-import { setHelloWorld } from "@/context/actions/example/hello-world";
-import {
-  XmasHackDispatchContext,
-  XmasHackStateContext,
-} from "@/context/context/context";
-import { FC, useContext } from "react";
+import { MainGame } from "@/components/templates/main-game";
+import { FC } from "react";
 
 const Home: FC = () => {
-  const dispatch = useContext(XmasHackDispatchContext);
-  const { helloWorld } = useContext(XmasHackStateContext);
-
-  const handleHelloWorld = () => {
-    dispatch(setHelloWorld(!helloWorld));
-  };
   return (
     <>
       <section className="min-h-[calc(100vh-64px)] bg-base-200">
-        <TimeCounter />
-        <button className="btn-primary btn" onClick={handleHelloWorld}>
-          Value of Hello World: "{helloWorld.toString()}" - click to change
-        </button>
+        <MainGame />
       </section>
     </>
   );
