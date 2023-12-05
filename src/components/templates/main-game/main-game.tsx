571641f1a9c2e7df3c3317160c648a3e092ae1be
Stephen Fairbanks <stephen.fairbanks@autotrader.co.uk>
2023-12-05 15:19:00 +0000

    fix: remove demo helloWorld

--- src/components/templates/main-game/main-game.tsx
+++ src/components/templates/main-game/main-game.tsx
@@ -1,31 +1,15 @@
 import { BasicCarCard } from "@/components/molecules/basic-card-component";
 import { Header } from "@/components/molecules/header/header";
 import { Footer } from "@/components/molecules/footer/footer";
-import { setHelloWorld } from "@/context/actions/example/hello-world";
-import {
-  XmasHackDispatchContext,
-  XmasHackStateContext,
-} from "@/context/context/context";
-import { useContext } from "react";
 import { useGenerateCards } from "@/utils/hooks/useGenerateCards.hooks";
 
 export const MainGame = () => {
-  const dispatch = useContext(XmasHackDispatchContext);
-  const { helloWorld } = useContext(XmasHackStateContext);
-  const handleHelloWorld = () => {
-    dispatch(setHelloWorld(!helloWorld));
-  };
-
   const { removeCarFromList, cars } = useGenerateCards();
 
   return (
     <>
       <Header />
       <div>
-        <button className="btn-primary btn" onClick={handleHelloWorld}>
-          Value of Hello World: "{helloWorld.toString()}" - click to change
-        </button>
-
         <div className="grid grid-cols-6 gap-2">
           {cars.map((car) => {
             return (
