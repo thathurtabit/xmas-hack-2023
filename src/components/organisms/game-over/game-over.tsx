70ae8f0bf4c895a0cde6a74fa330acd76daed497
Adam Rutherford-Shaw <adamrutherfordshaw@gmail.com>
2023-12-06 12:48:54 +0000

    fix: rewrite game over copy

--- src/components/organisms/game-over/game-over.tsx
+++ src/components/organisms/game-over/game-over.tsx
@@ -7,18 +7,19 @@ export const GameOver: FC<GameOverProps> = ({ reason }) => {
   const getReasonCopy = () => {
     switch (reason) {
       case GameOverReason.Bankrupt:
-        return "You ran out of money. Hope you don't need to pay rent.";
+        return "You ran out of money";
       case GameOverReason.Time:
-        return "You wasted two years of your life. Your parents must be so proud.";
+        return "You ran out of time";
     }
   }
   return (
     <section className="fixed top-10 right-10 left-10 bottom-10 flex flex-col items-center justify-center">
       <IconSad className="text-5xl animate-pulse mb-5" />
       <h2 className="text-6xl center font-[800] uppercase">Game over</h2>
-      <p>You failed. How pathetic.</p>
-      <p className="mb-10">{getReasonCopy()}</p>
+      <p>How pathetic. Your parents must be so proud.</p>
+      <p className="mb-10">Everyone was right about you.</p>
+      <p className="text-2xl center font-[300] uppercase mb-2">{getReasonCopy()}</p>
       <Button variant="primary" onClick={() => location.reload()}>Try again</Button>
-    </section>
+    </section >
   );
 };
