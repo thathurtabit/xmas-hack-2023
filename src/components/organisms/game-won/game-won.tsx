c56a5ff736013af1d9d2241fad60897fe6d3e141
Adam Rutherford-Shaw <adamrutherfordshaw@gmail.com>
2023-12-06 14:10:59 +0000

    feat: add score and restart button to game won component

--- src/components/organisms/game-won/game-won.tsx
+++ src/components/organisms/game-won/game-won.tsx
@@ -1,13 +1,18 @@
+import { Button } from "@/components/atoms/button/button";
 import { IconTrophy } from "@/components/icons/trophy/trophy";
 import { FC } from "react";
+import { GameWonProps } from "./game-won.types";
 
-export const GameWon: FC = () => {
+export const GameWon: FC<GameWonProps> = ({ score }) => {
+
   return (
     <section className="fixed top-10 right-10 left-10 bottom-10 flex flex-col items-center justify-center">
       <IconTrophy className="text-5xl animate-pulse mb-5" />
       <h2 className="text-6xl center font-[800] uppercase">You win</h2>
       <p>That&apos;s it.</p>
-      <p>Go home.</p>
+      <p className="mb-10">Go home.</p>
+      <p className="text-2xl center font-[300] uppercase mb-2">Your score was {score}</p>
+      <Button variant="primary" onClick={() => location.reload()}>Play again</Button>
     </section>
   );
 };
---
+++ src/components/organisms/game-won/game-won.types.ts
@@ -0,0 +1,3 @@
+export interface GameWonProps {
+    score: number
+}
