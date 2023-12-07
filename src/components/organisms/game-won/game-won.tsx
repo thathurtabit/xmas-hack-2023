d216814ce36209b288aa79120dbe5ce40922951d
Nick Bansal <nick.bansal@autotrader.co.uk>
2023-12-07 10:46:43 +0000

    updated the text color in win screen

--- src/components/organisms/game-won/game-won.tsx
+++ src/components/organisms/game-won/game-won.tsx
@@ -11,7 +11,7 @@ export const GameWon: FC<GameWonProps> = ({ score }) => {
       animate={{ opacity: 1, scale: 1 }}
       exit={{ opacity: 0, scale: 2 }}
       transition={{ duration: 0.5 }}
-      className="h-screen flex flex-col items-center justify-center bg-green-900"
+      className="h-screen flex flex-col items-center justify-center bg-green-900 text-white"
     >
       <IconTrophy className="text-5xl animate-pulse mb-5" />
       <h2 className="text-6xl center font-[800] uppercase">You win</h2>
