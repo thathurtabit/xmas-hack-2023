588ab99a3ce19b20deea9eab9caeea49c32820fd
Stephen Fairbanks <stephen.fairbanks@autotrader.co.uk>
2023-12-07 11:57:56 +0000

    fix: change current cash format

--- src/components/molecules/header/header.tsx
+++ src/components/molecules/header/header.tsx
@@ -1,13 +1,14 @@
 import { XmasHackStateContext } from "@/context/context/context";
 import { goalCash, startingCash } from "@/settings/settings";
+import { formatCurrency } from "@/utils/format-currency";
 import { useContext } from "react";
 
 export const Header = () => {
   const { moneyAmount } = useContext(XmasHackStateContext);
   const currentCash =
     moneyAmount === startingCash
-      ? `£${moneyAmount / 1000}k`
-      : `£${moneyAmount.toLocaleString()}`;
+      ? `£${formatCurrency(moneyAmount / 1000)}k`
+      : `£${formatCurrency(moneyAmount)}`;
 
   return (
     <header className="flex justify-between p-[32px] bg-cyan-500 text-black flex-wrap">
